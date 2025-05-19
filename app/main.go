eeea41df75d8fbfbe81ca2a001ced3a7d1c3fb67
z-skywalker <zhangwen.hit@gmail.com>
2025-05-19 09:15:55 +0000

    support windows service

--- app/main.go
+++ app/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "github.com/apernet/hysteria/app/v2/cmd"
+import (
+	"runtime"
+	"os"
+
+	"golang.org/x/sys/windows/svc"
+	"github.com/apernet/hysteria/app/v2/cmd"
+)
+
+type hysteriaService struct{}
+
+func (m *hysteriaService) Execute(args []string, r <-chan svc.ChangeRequest, s chan<- svc.Status) (bool, uint32) {
+	s <- svc.Status{State: svc.StartPending}
+
+	go func() {
+		cmd.Execute()
+	}()
+
+	s <- svc.Status{State: svc.Running, Accepts: svc.AcceptStop | svc.AcceptShutdown}
+
+	for c := range r {
+		switch c.Cmd {
+		case svc.Stop, svc.Shutdown:
+			s <- svc.Status{State: svc.Stopped}
+			os.Exit(0)
+			break
+		}
+	}
+
+	s <- svc.Status{State: svc.Stopped}
+	return false, 0
+}
 
 func main() {
+	if runtime.GOOS == "windows" {
+		isWinService, err := svc.IsWindowsService()
+		if err == nil && isWinService {
+			svc.Run("Hysteria", &hysteriaService{})
+			return
+		}
+	}
+
 	cmd.Execute()
 }
