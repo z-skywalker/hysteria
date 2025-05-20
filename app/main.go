dfbe3ba46f994e17000055c31253c383c626c8d2
z-skywalker <zhangwen.hit@gmail.com>
2025-05-20 01:19:26 +0000

    clean code

--- app/main.go
+++ app/main.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"log"
-	"os"
-)
-
 func main() {
-	f, _ := os.OpenFile("D:\\tools\\hysteria\\debug.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	log.SetOutput(f)
-
 	run()
 }
